d05719d876b4bc57f57173186b15d6f887e50c65
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-03-17 14:03:29 -0600

    ¨¨JSON.stringify & JSON.parse

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ const obj1 = {
     c: {
         d: 'd',
         e: 'e',
+    },
+    edithA() {
+        this.a = 'AAAA';
     }
 };
 
-//Shallow copy
-const obj2 = {};
+//JSON.parse & JSON.stringify
+const stringifiedComplexObj = JSON.stringify(obj1);
+const obj2 = JSON.parse(stringifiedComplexObj);
 
-for (prop in obj1) {
-    obj2[prop] = obj1[prop];
-}
-
-const obj3 = Object.assign({}, obj1);
-const obj4 = Object.create(obj1);
+// it doesn't work with methods
