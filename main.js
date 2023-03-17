48ebf7167961f85e1208c940331eaed9e85de842
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-03-17 12:51:19 -0600

    Shallow copy

--- main.js
+++ main.js
@@ -1,60 +1,18 @@
-const jona = {
-    name: 'Jonathan',
-    age: 18,
-    approvedCourses: ['Course 1'],
-    addCourse(newCourse) {
-        console.log('This', this);
-        console.log('This.approvedCourse', this.approvedCourses);
-        this.approvedCourses.push(newCourse);
+const obj1 = {
+    a: 'a',
+    b: 'b',
+    c: {
+        d: 'd',
+        e: 'e',
     }
 };
 
-Object.seal(jona); //? Locks deleting (configurable)
-// Object.freeze(jona); //? Locks deleting and editing (configurable & writable)
+//Shallow copy
+const obj2 = {};
 
-console.log(Object.getOwnPropertyDescriptors(jona));
+for (prop in obj1) {
+    obj2[prop] = obj1[prop];
+}
 
-/*  enumerable: //? If this property is shown up during enumeration
-    writable: //? if this property can be edited
-    configurable: //? If this property can be deletable
-*/
-
-// console.log(Object.keys(jona));
-// console.log(Object.getOwnPropertyNames(jona));
-// console.log(Object.entries(jona));
-
-    ///  .defineProperty(1,2,3) ///
-
-    //* 1st argument: Object to select 
-    //! 2nd argument: name of property 
-    //? 3rd argument: {properties}
-    
-/* Object.defineProperty(jona, 'navigator', {
-    value: 'Chrome',
-    enumerable: false,
-    writable: true,
-    configurable: true,
-});
-
-Object.defineProperty(jona, 'editor', {
-    value: 'VSCode',
-    enumerable: true,
-    writable: false,
-    configurable: true,
-});
-
-Object.defineProperty(jona, 'terminal', {
-    value: 'WSL',
-    enumerable: true,
-    writable: true,
-    configurable: false,
-});
-
-Object.defineProperty(jona, 'nasaTest', {
-    value: 'Aliens',
-    enumerable: false,
-    writable: false,
-    configurable: false,
-});
-
-console.log(Object.getOwnPropertyDescriptors(jona)); */
+const obj3 = Object.assign({}, obj1);
+const obj4 = Object.create(obj1);
