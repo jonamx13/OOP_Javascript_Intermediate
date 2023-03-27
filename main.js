fd212081195c7f83d58969aa03cc0e8dbeaabb58
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-03-26 22:46:50 -0700

    Static methods

--- main.js
+++ main.js
@@ -38,6 +38,41 @@ function deepCopy(subject) {
     return copySubject;
 }
 
+function SuperObject() {}
+
+SuperObject.isObject = function (subject) {
+        return typeof subject == 'object';
+}
+SuperObject.deepCopy = function (subject) {
+        let copySubject;
+    
+        const subjectIsArray = isArray(subject);
+        const subjectIsObject = isObject(subject);
+    
+        if(subjectIsArray) {
+            copySubject = [];
+        } else if (subjectIsObject) {
+            copySubject = {};
+        } else {
+            return subject;
+        }
+    
+        for (key in subject) { 
+            const keyIsObject = isObject(subject[key]);
+    
+            if (keyIsObject) {
+                copySubject[key] = deepCopy(subject[key]);
+            } else {
+                if (subjectIsArray) {
+                    copySubject.push(subject[key]);
+                } else {
+                    copySubject[key] = subject[key];
+                }
+            }
+        } 
+    
+        return copySubject;
+}
 
 function requiredParam(param) {
     throw new Error(`${param} is required`);
